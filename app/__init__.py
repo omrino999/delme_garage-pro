7eaf440d288513ed5b54afb6c46c7d6b80ab4a6e
Omri Shitrit <omri.shitrit@yahoo.com>
2026-01-18 21:38:15 +0200

    Fix: set default database URI to SQLite if DATABASE_URL is not set

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__,
             template_folder='../templates', 
             static_folder='../static')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///garage.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev_secret_key')
 
